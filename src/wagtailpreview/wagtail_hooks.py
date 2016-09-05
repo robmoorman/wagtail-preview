9972d43d4a12832ed5a3b54392a4bcf322178d1f
Rob Moorman <rob@moori.nl>
2016-09-05 23:22:31 +0200

    Added custom javascript file

--- src/wagtailpreview/wagtail_hooks.py
+++ src/wagtailpreview/wagtail_hooks.py
@@ -1,1 +1,14 @@
-# Custom JavaScript goes here.
+from django.conf import settings
+from django.utils.html import format_html_join
+from wagtail.wagtailcore import hooks
+
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    js_files = [
+        'wagtailpreview/js/preview.js',
+    ]
+    js_includes = format_html_join('\n', '<script src="{0}{1}"></script>',
+        ((settings.STATIC_URL, filename) for filename in js_files)
+    )
+    return js_includes
